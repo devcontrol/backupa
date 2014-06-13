8c9d7436e1c75a9061ac7ae234eb5e82fe0a87f2
zwarag <harrys.kavan@gmail.com>
2014-06-14 01:00:28 +0200

    v1.0 stable

--- backupa.sh
+++ backupa.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# something 
+# backupscript
 
-#BASE URL
-BASEURL="/mnt/alias/backup/"
-MAILURL="/mnt/rack/0302/shishaMail/"
-CLOUDURL="/mnt/rack/0302/owncloud"
+BASEURL="/mnt/alias/backup"
+MAILURL="/mnt/alias/storage/shishaMail"
+CLOUDURL="/mnt/alias/storage/owncloud"
 
 DAY=`eval date +"%d"`
 MONTH=`eval date +"%m"`
@@ -16,49 +15,48 @@ PIMPURL=$BASEURL/$YEAR/$MONTH/$DAY
 
 COMMAND="cd $PIMPURL"
 $COMMAND 2>/dev/null
-#`eval $COMMAND` 2>/dev/null
 
 if [ $? -eq 0 ]
 then
-		echo "Command 1 was successful"
+                echo "Command 1 was successful"
 else
-		echo "There ain't folders!!"
-		echo "I'll mkdir some!"
+                echo "There ain't folders!!"
+                echo "I'll mkdir some!"
 
-		COMMAND="mkdir -p $PIMPURL"
-		$COMMAND 2>/dev/null
-		#`eval $COMMAND` 2>/dev/null
+                COMMAND="mkdir -p $PIMPURL"
+                $COMMAND 2>/dev/null
 fi
 if [ $? -eq 0 ]
 then
-		echo 'Command 2 was successful'
-		echo "STARTING MAIL BACKUP"
+                echo 'Command 2 was successful'
+                echo "STARTING MAIL BACKUP"
 
-		COMMAND='tar cfvz '$PIMPURL'shisha_'$HOUR'_'$MINUTE'_.data.tar.gz '$MAILURL
-		$COMMAND 2>/dev/null
+                COMMAND="tar cfvz ${PIMPURL}shisha_${HOUR}_${MINUTE}_.data.tar.gz ${MAILURL}"
+                $COMMAND 2>/dev/null
 
-		if [ $? -eq 0 ]
-		then
-				echo 'MAIL BACKUP SUCCESSFUL'L
-		else
-				echo "FAILURE!!"
-		fi
+                if [ $? -eq 0 ]
+                then
+                                echo 'MAIL BACKUP SUCCESSFUL'L
+                else
+                                echo "FAILURE!!"
+                fi
 
-		HOUR=`eval date +"%H"`
-		MINUTE=`eval date +"%M"`
+                HOUR=`eval date +"%H"`
+                MINUTE=`eval date +"%M"`
 
-		echo "STARTING CLOUD BACKUP"
+                echo "STARTING CLOUD BACKUP"
 
-		COMMAND='tar cfvz '$PIMPURL'cloud_'$HOUR'_'$MINUTE'_.data.tar.gz ' $$CLOUDURL
-		$COMMAND 2>/dev/null
+                COMMAND="tar cfvz ${PIMPURL}cloud_${HOUR}_${MINUTE}_.data.tar.gz ${CLOUDURL}"
+                $COMMAND 2>/dev/null
 
-		if [ $? -eq 0 ]
-		then
-				echo 'CLOUD BACKUP SUCCESSFUL'L
-		else
-				echo "FAILURE!!"
-		fi
+                if [ $? -eq 0 ]
+                then
+                                echo 'CLOUD BACKUP SUCCESSFUL'L
+                else
+                                echo "FAILURE!!"
+                fi
 
 else
-		echo 'Nope! Must be some kind of strage Err0r!!'
+                echo 'Nope! Must be some kind of strage Err0r!!'
 fi
+
